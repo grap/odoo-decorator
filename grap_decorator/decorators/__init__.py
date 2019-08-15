ddd2effec57a757feef31cbacf2fbf7e877b678f
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-08-15 15:29:40 +0200

    fixup! fixup! wip

--- grap_decorator/decorators/__init__.py
+++ grap_decorator/decorators/__init__.py
@@ -15,46 +15,24 @@ def setup_models(self, cr):
 
     res = _setup_models_original(self, cr)
     env = api.Environment(cr, SUPERUSER_ID, {})
+
+    print("2)~~~~~~~~~~")
+    print(")~~~~~~~~~~overloaded setup_model")
+
     models = list(env.values())
     for model in models:
+        if model._name == 'account.check.deposit':
+            import pdb; pdb.set_trace()
         print(model._name)
+
     return res
 
 Registry.setup_models = setup_models
-
-# from odoo import registry
-
-# import pdb; pdb.set_trace()
-# class AbstractModel(models.AbstractModel):
-#     # def __init__(self, pool, cr):
-#     #     print("__init__.py")
-#     #     return super().__init__(pool, cr)
-
-#     @classmethod
-#     def _build_model(cls, pool, cr):
-#         print("====================")
-#         print("_build_model")
-#         res = super(AbstractModel, cls)._build_model(pool, cr)
-#         print("**********************")
-#         return res
-
-#     @classmethod
-#     def _build_model_attributes(cls, pool):
-#         print("====================")
-#         print("_build_model_attributes")
-#         res = super(AbstractModel, cls)._build_model_attributes(pool)
-#         print("**********************")
-#         return res
 
 
 def check_state(allowed_states, display_field='name'):
     def wrap(func):
         def wrapped_function(self, *args, **kwargs):
-            print("========================")
-            print("========================")
-            print("========================")
-            print("CHECKING STATE %s...." % ','.join(allowed_states))
-            import pdb; pdb.set_trace()
             undeletable_items = self.filtered(
                 lambda x: x.state not in allowed_states)
             if undeletable_items:
@@ -67,14 +45,12 @@ def check_state(allowed_states, display_field='name'):
                         'allowed_state_names': ', '.join(allowed_states),
                 })
             return func(self, *args, **kwargs)
-        print("~~~~~~~~~~")
+        print("1)~~~~~~~~~~")
         print("~~~~~~~~~~wrapped_function")
-        # attrsetter('_check_states', func)
         # attach allowed_states value to the function, to
         # allow post-process, once the class is initialized
         setattr(func, '_check_states', allowed_states)
+        import pdb; pdb.set_trace()
 
         return wrapped_function
-    print("~~~~~~~~~~")
-    print("~~~~~~~~~~check_state")
     return wrap
